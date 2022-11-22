aac29b6a876af9dd01a6e46da14edf22cf8151aa
caseinpoint <drues.other.email@gmail.com>
2022-11-22 09:56:33 -0800

    start on getting api data

--- random/generate_seed.py
+++ random/generate_seed.py
@@ -1,8 +1,15 @@
-from json import loads
+from json import dump, load, loads
+from pprint import pprint
 import random
 from re import compile, IGNORECASE
+from requests import get
 from secrets import choice
 from subprocess import run
+
+API_URL = 'https://api.weather.gov'
+API_HEADERS = {'User-Agent': '(randomseedapp.py, drues.other.email@gmail.com)'}
+with open('./calStations.json') as f:
+    CAL_STATIONS = load(f)
 
 RE_SIGNAL = compile(r'Signal level=(?P<signal>-\d+)', IGNORECASE)
 # RE_ADDRESS = compile(r'Address:\s*(?P<address>\S+)', IGNORECASE)
@@ -52,16 +59,42 @@ def get_cpu_seed(num_bytes=64):
     return random.randbytes(num_bytes)
 
 
+def ca_stations():
+    ca_zones_query_res = get(url=API_URL+'/zones/public',
+                                headers=API_HEADERS,
+                                params={'area': 'CA'})
+    ca_zones_data = ca_zones_query_res.json()
+    stations = []
+    for feature in ca_zones_data['features']:
+        for station in feature['properties']['observationStations']:
+            stations.append(station)
+    with open('./calStations.json', 'w') as f:
+        dump(stations, f)
+
+
+def get_weather_seed(num_bytes=64):
+    station_url = choice(CAL_STATIONS)
+
+    station_query_res = get(url=station_url+'/observations',
+                            headers=API_HEADERS,
+                            params={'limit': 1})
+    station_data = station_query_res.json()
+    pprint(station_data)
+
+
 if __name__ == '__main__':
-    print('test output:')
+    # print('test output:')
 
-    cpu_seed = get_cpu_seed(num_bytes=32)
-    print(f'cpu_seed: {cpu_seed.hex()}')
+    # cpu_seed = get_cpu_seed(num_bytes=64//3)
+    # print(f'cpu_seed: {cpu_seed.hex()}')
 
-    wifi_seed = get_wifi_seed(num_bytes=32)
-    print(f'wifi_seed: {wifi_seed.hex()}')
+    # wifi_seed = get_wifi_seed(num_bytes=64//3)
+    # print(f'wifi_seed: {wifi_seed.hex()}')
 
-    random.seed(cpu_seed + wifi_seed)
+    # random.seed(cpu_seed + wifi_seed)
 
-    rand_ints = [random.randint(1,100) for _ in range(10)]
-    print(f'rand_ints: {rand_ints}')
+    # rand_ints = [random.randint(1,100) for _ in range(10)]
+    # print(f'rand_ints: {rand_ints}')
+
+    # ca_stations()
+    get_weather_seed(num_bytes=64//3)
